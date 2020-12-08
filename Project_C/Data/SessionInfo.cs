ab7c6d6cf42c43f2769ad0eb184f426155222170
Ferdi <33029115+DatKenwayGuy@users.noreply.github.com>
2020-12-08 17:03:36 +0100

    Migration UserMeeting
    De migration files van het toevoegen van de UserMeeting table in de Database.

--- Project_C/Data/SessionInfo.cs
+++ Project_C/Data/SessionInfo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Project_C.Models;
 
 namespace Project_C.Data
 {
