56e476af7eec57c2b29495f0ac7b4dbfc49efde2
Darshan Alakhramsing <AtlasFlame_@hotmail.com>
2021-04-03 01:47:22 +0200

    Made some changed to the AcceptSessionTest automation code

---
+++ AcceptSessionAutomationTest/UnitTest1.cs
@@ -0,0 +1,64 @@
+using System;
+using Xunit;
+using Project_C.Models;
+using Project_C.Data;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace AcceptSessionAutomationTest
+{
+    public class UnitTest1 : IDisposable
+    {
+        public readonly IWebDriver driver;
+        private readonly Thread server;
+
+        public UnitTest1()
+        {
+            var assembly = Assembly.Load("Project_C");
+
+            server = new Thread(() =>
+            {
+                try
+                {
+                    assembly.EntryPoint.Invoke(null, new object[] { new string[0] });
+                }
+                catch
+                {
+                    // Ignore exception
+                }
+            });
+            server.Start();
+
+        }
+
+        public void Dispose()
+        {
+
+            server.Interrupt();
+            server.Join();
+            driver?.Dispose();
+        }
+
+        [Fact]
+        public void Driver()
+        {
+
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(115));
+                driver.Navigate().GoToUrl("https://localhost:5001/");
+                driver.FindElement(By.Name("Input.Email")).SendKeys("darshan@cimsolutions.nl");
+                driver.FindElement(By.Name("Input.Password")).SendKeys("Darshan12345!" + Keys.Enter);
+                driver.FindElement(By.Id("SessieOverzicht")).Click();
+                wait.Until(e => e.FindElement(By.Id("Header")));
+            }
+        }
+    }
+}
