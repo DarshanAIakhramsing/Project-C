aca76ca1fd54b563c3b1af6d52a6f573af60cdf9
Ferdi <33029115+DatKenwayGuy@users.noreply.github.com>
2021-04-07 13:07:40 +0200

    Usability verandering
    Verandering in het zoeken van medewerkers, datum bij sessie aanmaken is nu altijd de datum van vandaag en je krijgt een melding als je een verandering hebt gemaakt in de sessie edit.

---
+++ Project_C_Tests/EmployeeTest.cs
@@ -0,0 +1,30 @@
+using System;
+using Xunit;
+using System.Linq;
+using Project_C;
+using Project_C.Data;
+using Project_C.Pages;
+using Project_C.Services;
+using System.Collections.Generic;
+using Project_C.Models;
+
+
+namespace Project_C_Tests
+{
+    public class EmployeeTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            List<User> expected = new List<User>();
+
+            // Act
+            var result = UserService.DisplayEmail("ferdi@cimsolutions.nl");
+
+            // Assert
+            Assert.Equal(expected, result);
+
+        }
+    }
+}
